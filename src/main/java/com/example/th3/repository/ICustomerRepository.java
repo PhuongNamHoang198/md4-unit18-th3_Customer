1cbd5f73732d7945175125e06d427de3b404b2b4
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-09-28 21:39:28 +0700

    done and bug post put

--- src/main/java/com/example/th3/repository/ICustomerRepository.java
+++ src/main/java/com/example/th3/repository/ICustomerRepository.java
@@ -2,9 +2,11 @@ package com.example.th3.repository;
 
 import com.example.th3.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICustomerRepository  extends JpaRepository<Customer, Long> {
+public interface ICustomerRepository  extends PagingAndSortingRepository<Customer, Long> {
 }
 
